c150d2dfddce2389c902ec6d2e0c0ca3afdbd0e2
Nawang06 <nawangla09@gmail.com>
2021-08-19 15:41:48 +0530

    Modified to add High Score Feature

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -8,20 +8,27 @@ class Score (Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.make_score()
 
     def make_score(self, points=0):
+        self.clear()
         self.hideturtle()
         self.penup()
         self.color("white")
         self.goto(0, 260)
-        self.write(f"Score = {self.score}", align=ALIGNMENT, font=ALIGNMENT)
+        self.write(f"Score = {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def update_score (self):
+    def update_score(self):
         self.score += 1
-        self.clear()
         self.make_score(self.score)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.make_score()
