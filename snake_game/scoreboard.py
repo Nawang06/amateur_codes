7ddce44a836951b6347eb5b596fd40980ab6245a
Nawang06 <nawangla09@gmail.com>
2021-08-19 16:00:34 +0530

    Modification to scoreboard.py script

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -1,5 +1,7 @@
 from turtle import Turtle
 
+with open ("data.txt") as data:
+    high = data.read()
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
@@ -8,7 +10,7 @@ class Score (Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = int(high)
         self.make_score()
 
     def make_score(self, points=0):
@@ -30,5 +32,7 @@ class Score (Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(str(self.high_score))
         self.score = 0
         self.make_score()
